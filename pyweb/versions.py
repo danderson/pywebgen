049b4898904563108f4ff7b3f100859dc72b8c3d
David Anderson <dave@natulte.net>
2008-12-19 20:25:32 +0100

    Make link management generic, and implement a 'latest' symlink.
    The latest symlink is updated on every generation to point to the new
    generation.

--- pyweb/versions.py
+++ pyweb/versions.py
@@ -34,8 +34,13 @@ _TS_RE = re.compile(_TS_RE_FORM)
 _MANIFEST_RE = re.compile(_TS_RE_FORM + r'\.MANIFEST')
 
 
-class InvalidCurrentLinkError(error.Error):
-    """The 'current' symlink is not valid."""
+# The name of the various symlinks we maintain.
+_CURRENT_LINK = 'current'
+_LATEST_LINK = 'latest'
+
+
+class InvalidLinkError(error.Error):
+    """Version symlink is not valid."""
 
 
 class NoVersionsError(error.Error):
@@ -45,7 +50,6 @@ class NoVersionsError(error.Error):
 class VersionnedGenerator(object):
     def __init__(self, output_root):
         self._output_root = os.path.abspath(output_root)
-        self._current_link = os.path.join(self._output_root, 'current')
         util.CreateDir(self._output_root)
 
     def _FindTimestamps(self):
@@ -57,33 +61,37 @@ class VersionnedGenerator(object):
         timestamps.sort(reverse=True)
         return timestamps
 
-    def _CurrentTimestamp(self):
-        if not self._CurrentExists():
+    def _LinkLocation(self, link):
+        return os.path.join(self._output_root, link)
+
+    def _LinkTimestamp(self, link):
+        if not self._LinkExists(link):
             return None
 
         # Resolve the symlink and grab the timestamp
         #
         # TODO(dave): Detect if the symlink points outside the output
         # dir, which would be an invalid symlink.
-        pointed_path = os.path.realpath(self._current_link)
+        pointed_path = os.path.realpath(self._LinkLocation(link))
         pointed_ts = os.path.basename(pointed_path)
         if not _TS_RE.match(pointed_ts):
-            raise InvalidCurrentLinkError(self._current_link)
+            raise InvalidLinkError(link)
         return pointed_ts
 
-    def _CurrentExists(self):
-        if not os.path.exists(self._current_link):
+    def _LinkExists(self, link):
+        link_path = self._LinkLocation(link)
+        if not os.path.exists(link_path):
             return False
-        elif not os.path.islink(self._current_link):
-            raise InvalidCurrentLinkError(self._current_link)
+        elif not os.path.islink(link_path):
+            raise InvalidLinkError(link)
         return True
 
-    def _SetCurrent(self, ts):
-        if self._CurrentExists():
-            os.remove(self._current_link)
+    def _SetLink(self, link, ts):
+        link_path = self._LinkLocation(link)
+        if self._LinkExists(link):
+            os.remove(link_path)
 
-        os.symlink(os.path.join(self._output_root, ts),
-                   self._current_link)
+        os.symlink(os.path.join(self._output_root, ts), link_path)
 
     def Generate(self, input_root, use_processors):
         ts = time.localtime()
@@ -94,8 +102,10 @@ class VersionnedGenerator(object):
         generator.Generator(input_root, use_processors).Generate(
             out_dir, timestamp=ts, manifest_path=manifest_file)
 
-        if not self._CurrentExists():
-            self._SetCurrent(ts_str)
+        self._SetLink(_LATEST_LINK, ts_str)
+
+        if not self._LinkExists(_CURRENT_LINK):
+            self._SetLink(_CURRENT_LINK, ts_str)
             current = True
         else:
             current = False
@@ -104,7 +114,7 @@ class VersionnedGenerator(object):
 
     def Versions(self):
         ts = self._FindTimestamps()
-        current = self._CurrentTimestamp()
+        current = self._LinkTimestamp(_CURRENT_LINK)
         return ts, current
 
     def ChangeCurrent(self, version=0):
@@ -115,20 +125,20 @@ class VersionnedGenerator(object):
             raise ArgumentError('Invalid version %d specified, '
                                 'only %d available' % (version, len(ts)))
 
-        current = self._CurrentTimestamp()
+        current = self._LinkTimestamp(_CURRENT_LINK)
 
         if current and current == ts[version]:
             # Nothing to do, we're current already.
             return
 
         # (re)point the symlink
-        self._SetCurrent(ts[version])
+        self._SetLink(_CURRENT_LINK, ts[version])
 
         return ts[version]
 
     def GarbageCollect(self):
         ts = self._FindTimestamps()
-        current = self._CurrentTimestamp()
+        current = self._LinkTimestamp(_CURRENT_LINK)
 
         # If there is no current pointer, we don't know what to GC.
         if not current:
