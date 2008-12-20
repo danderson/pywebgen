342943f0d40eda23566cf1a0e9cea0449de95f65
David Anderson <dave@natulte.net>
2008-12-20 04:51:51 +0100

    Add get_local_yaml.sh to retrieve python-yaml for systems lacking it.

---
+++ get_local_yaml.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Copyright 2008 David Anderson <dave@natulte.net>
+#
+# Redistribution of this file is permitted under
+# the terms of the GNU Public License (GPL) version 2.
+#
+# Retrieve a local version of PyYaml. This is useful if you don't have
+# PyYaml available in your distro, or want a self-contained pyweb.
+
+svn co http://svn.pyyaml.org/pyyaml/trunk/lib/yaml/
